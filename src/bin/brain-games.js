e0556aa43f1c6e124e53240bdbd400a2d9aa2f06
euhoo <euhoo@mail.ru>
2019-01-14 21:21:12 +0300

    make second checkpoint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import nameRequest from '..';
+console.log(nameRequest());
---
+++ src/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const nameRequest=()=> {
+console.log('Welcome to the Brain Games!');
+console.log();
+const name = readlineSync.question('May i have your name? ');
+return console.log(`Hello, ${name}!`);
+}
+export default nameRequest;
