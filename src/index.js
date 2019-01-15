f97c125cdd9b1e63dd477df4cfe68d6ac913d6eb
euhoo <euhoo@mail.ru>
2019-01-15 19:58:51 +0300

    travis works!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const nameRequest = () => {
   console.log('Welcome to the Brain Games!');
   console.log();
   const name = readlineSync.question('May i have your name? ');
-  const text=`Hello, ${name}!`
+  const text = `Hello, ${name}!`;
   return text;
 };
 
