dc7a8392fd99f7f9c5537498f4d3f5cb2b2407e3
euhoo <euhoo@mail.ru>
2019-01-15 19:19:12 +0300

    clean

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import nameRequest from '..';
 
-nameRequest();
+console.log(nameRequest());
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const nameRequest = () => {
   console.log('Welcome to the Brain Games!');
   console.log();
   const name = readlineSync.question('May i have your name? ');
-  return console.log(`Hello, ${name}!`);
+  const text=`Hello, ${name}!`
+  return text;
 };
 
 export default nameRequest;
