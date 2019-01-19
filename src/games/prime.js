c3399e0f87efb829e17e8a33bd2b5d6bf63ce2c2
euhoo <euhoo@mail.ru>
2019-01-19 10:48:24 +0300

    realized better prime algorithm

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,22 +6,19 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const smallestPrime = 2;
 
 const isPrime = (question) => {
-  let correctAnswer = 'yes';
   if (question < smallestPrime) {
-    correctAnswer = 'no';
-    return correctAnswer;
+    return 'no';
   }
-  for (let i = question - 1; i > smallestPrime - 1; i -= 1) {
+  for (let i = smallestPrime; i <= Math.floor(Math.sqrt(question)); i += 1) {
     if (question % i === 0) {
-      correctAnswer = 'no';
-      break;
+      return 'no';
     }
   }
-  return correctAnswer;
+  return 'yes';
 };
 
 const logic = () => {
-  const question = makeRandomNumber(1, 90);
+  const question = makeRandomNumber(1, 99);
   const correctAnswer = isPrime(question);
   const means = [question, correctAnswer];
   return means;
